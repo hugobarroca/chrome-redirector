62f428598a430aa73cbe7e2faae138cb038ca0c4
Hugo Barroca <hugobarroca98@gmail.com>
2024-02-09 21:46:27 +0000

    ✨ Redirected websites can now be configured from options

--- options.js
+++ options.js
@@ -1,61 +1,73 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var myButton = document.getElementById("save");
-  myButton.addEventListener("click", function () {
-    saveOptions();
-  });
-  restoreOptions();
-});
-
-const restoreOptions = () => {
-  chrome.storage.sync.get("sitesToRedirect").then((result) => {
-    updateSitesList(result.sitesToRedirect);
-  });
-};
+document.addEventListener('DOMContentLoaded', function () {
+  var myButton = document.getElementById('save')
+  myButton.addEventListener('click', function () {
+    saveOptions()
+  })
+  createListWithSavedOptions()
+})
 
 const saveOptions = () => {
-  const websiteToRedirect = document.getElementById("websiteToRedirect").value;
-  console.log("Website value: " + websiteToRedirect);
-  chrome.storage.sync.get("sitesToRedirect").then((oldSites) => {
-    newList = [...oldSites.sitesToRedirect, websiteToRedirect];
-    chrome.storage.sync.set({ sitesToRedirect: newList }, () => {
-      updateSitesList(newList);
-    });
-  });
-};
+  const websiteToRedirect = document.getElementById('websiteToRedirect').value
+  console.log('Website value: ' + websiteToRedirect)
+  chrome.storage.sync.get('sitesToRedirect').then((oldSites) => {
+    console.log(oldSites)
+    if (isEmpty(oldSites)) {
+      newList = [websiteToRedirect]
+      chrome.storage.sync.set({ sitesToRedirect: newList }, () => {
+        createListOfRedirectedSites(newList)
+      })
+    } else {
+      newList = [...oldSites?.sitesToRedirect, websiteToRedirect]
+      chrome.storage.sync.set({ sitesToRedirect: newList }, () => {
+        createListOfRedirectedSites(newList)
+      })
+    }
+  })
+}
+
+const createListWithSavedOptions = () => {
+  chrome.storage.sync.get('sitesToRedirect').then((result) => {
+    createListOfRedirectedSites(result.sitesToRedirect)
+  })
+}
 
 const addValue = (newValue) => {
-  chrome.storage.sync.get("sitesToRedirect").then((oldSites) => {
-    newList = [...oldSites.sitesToRedirect, newValue];
+  chrome.storage.sync.get('sitesToRedirect').then((oldSites) => {
+    newList = [...oldSites.sitesToRedirect, newValue]
     chrome.storage.sync.set({ sitesToRedirect: newList }, () => {
-      updateSitesList(newList);
-    });
-  });
-};
+      createListOfRedirectedSites(newList)
+    })
+  })
+}
 
 const removeValue = (keyToRemove) => {
-  chrome.storage.sync.get("sitesToRedirect").then((oldSites) => {
-    newList = [...oldSites.sitesToRedirect.filter((x) => x !== keyToRemove)];
+  chrome.storage.sync.get('sitesToRedirect').then((oldSites) => {
+    newList = [...oldSites.sitesToRedirect.filter((x) => x !== keyToRemove)]
     chrome.storage.sync.set({ sitesToRedirect: newList }, () => {
-      updateSitesList(newList);
-    });
-  });
-};
+      createListOfRedirectedSites(newList)
+    })
+  })
+}
 
-const updateSitesList = (items) => {
-  console.log("Running create list...");
-  var listContainer = document.getElementById("listContainer");
-  listContainer.innerHTML = "";
-  var ul = document.createElement("ul");
-  items.forEach(function (item) {
-    var li = document.createElement("li");
-    var removeButton = document.createElement("button");
-    removeButton.textContent = "Remove";
-    removeButton.addEventListener("click", function () {
-      console.log("Remove button clicked!");
-      removeValue(item);
-    });
-    li.textContent = item;
-    ul.appendChild(li).append(removeButton);
-  });
-  listContainer.appendChild(ul);
-};
+const createListOfRedirectedSites = (websitesToRedirect) => {
+  console.log('Running create list...')
+  var listContainer = document.getElementById('listContainer')
+  listContainer.innerHTML = ''
+  var ul = document.createElement('ul')
+  websitesToRedirect.forEach(function (item) {
+    var li = document.createElement('li')
+    var removeButton = document.createElement('button')
+    removeButton.textContent = 'Remove'
+    removeButton.addEventListener('click', function () {
+      console.log('Remove button clicked!')
+      removeValue(item)
+    })
+    li.textContent = item
+    ul.appendChild(li).append(removeButton)
+  })
+  listContainer.appendChild(ul)
+}
+
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0
+}
--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,8 @@
-window.addEventListener("yt-navigate-finish", (e) => {
-  console.log("Youtube URL changed", e);
-  window.location = "https://www.duolingo.com/learn";
-});
-
-window.location = "https://www.duolingo.com/learn";
+chrome.storage.sync.get('sitesToRedirect').then((sitesToRedirect) => {
+  let condition = sitesToRedirect.sitesToRedirect.includes(window.location.href) 
+                  || sitesToRedirect.sitesToRedirect.includes(window.location.origin)
+                  || sitesToRedirect.sitesToRedirect.includes(window.location.host);
+  if (condition) {
+    window.location = 'https://www.duolingo.com/learn'
+  }
+})
