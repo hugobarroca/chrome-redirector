e85664917f40eae61e9371900065f44bb7a915bc
Hugo Barroca <52352480+hugobarroca@users.noreply.github.com>
2024-02-11 22:26:58 +0000

    ✨ Add disable function to the popup. (#1)
    * ✨ Added disable function to the popup.
    
    * ✨ Temporary disabling the extension now works.

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,11 @@
-chrome.storage.sync.get('sitesToRedirect').then((sitesToRedirect) => {
-  let condition = sitesToRedirect.sitesToRedirect.includes(window.location.href) 
-                  || sitesToRedirect.sitesToRedirect.includes(window.location.origin)
-                  || sitesToRedirect.sitesToRedirect.includes(window.location.host);
-  if (condition) {
-    window.location = 'https://www.duolingo.com/learn'
+chrome.storage.sync.get(["sitesToRedirect", "disabled"]).then((data) => {
+  const { sitesToRedirect, disabled } = data;
+  const shouldRedirect =
+    !disabled &&
+    [window.location.href, window.location.origin, window.location.host].some(
+      (url) => sitesToRedirect.includes(url)
+    );
+  if (shouldRedirect) {
+    window.location = "https://www.duolingo.com/learn";
   }
-})
+});
---
+++ scripts/popup.js
@@ -0,0 +1,71 @@
+document.getElementById("options").addEventListener("click", function () {
+  chrome.tabs.create({ url: "options.html" });
+});
+
+function updateEnableButton() {
+  chrome.storage.sync.get("disabled", (data) => {
+    const disableButton = document.getElementById("disable");
+    if (data.disabled) {
+      disableButton.textContent = "Enable";
+      disableButton.addEventListener("click", function () {
+        chrome.storage.sync.set({ disabled: false }, () => {
+          console.log("Enabled");
+        });
+      });
+    } else {
+      disableButton.textContent = "Disable";
+      disableButton.addEventListener("click", disableButtonFunctionality);
+    }
+  });
+}
+
+function updateTimer() {
+  chrome.alarms.get("enable", (alarm) => {
+    if (alarm) {
+      const timerDisplay = document.getElementById("timerDisplay");
+      const timeRemaining = alarm.scheduledTime - Date.now();
+      const minutesRemaining = Math.floor(timeRemaining / 60000);
+      const secondsRemaining = Math.floor((timeRemaining % 60000) / 1000);
+      timerDisplay.textContent = `Time remaining: ${minutesRemaining}:${secondsRemaining}`;
+    }
+  });
+}
+
+function updateEnableLabel() {
+  chrome.storage.sync.get("disabled", (data) => {
+    const disabledLabel = document.getElementById("disabledLabel");
+    if (data.disabled) {
+      disabledLabel.textContent = "Disabled";
+      disabledLabel.style.color = "red";
+    } else {
+      disabledLabel.textContent = "Enabled";
+      disabledLabel.style.color = "green";
+    }
+  });
+}
+
+// If an alarm with the name "enable" exists, it sets the value of the timerDisplay element to the time remaining until the alarm triggers
+function updateDisplay() {
+  updateTimer();
+  updateEnableLabel();
+  updateEnableButton();
+}
+
+// Updates display when the popup is opened
+updateDisplay();
+
+// Update the timer display every second
+setInterval(updateDisplay, 1000);
+
+// Disables the extension and sets an alarm to enable the extension after a specified amount of time
+function disableButtonFunctionality() {
+  chrome.storage.sync.set({ disabled: true }, () => {
+    let disableTimeInMinutes = parseInt(
+      document.getElementById("disableTime").value
+    );
+
+    chrome.alarms.create("enable", {
+      delayInMinutes: disableTimeInMinutes, // Convert disableTimeInMinutes to a number
+    });
+  });
+}
