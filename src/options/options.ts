8af428d4f9eeefe62b3eba9bca787fba1f2f62d6
Hugo Barroca <52352480+hugobarroca@users.noreply.github.com>
2024-07-08 21:32:01 +0100

    Feature/vite (#2)
    * ♻️ Converted options.js to typescript
    
    * 🔧 Added Vite as a build tool.
    
    * 🐛 Fixed Vite build process not working as expected.

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,16 +1,31 @@
 // Startup code for the options page
 document.addEventListener("DOMContentLoaded", function () {
   let addButton = document.getElementById("add-blocked-website-button");
+  if (!addButton) {
+    console.error("Add button is null");
+    return;
+  }
   addButton.addEventListener("click", addWebsite);
   createListWithSavedOptions();
 });
 
 const clickButtonOnEnter = () => {
   let input = document.getElementById("add-website-input");
+  if (!input) {
+    console.error("Input is null");
+    return;
+  }
   input.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
       event.preventDefault();
-      document.getElementById("add-blocked-website-button").click();
+      const addBlockedWebsiteButton = document.getElementById(
+        "add-blocked-website-button"
+      );
+      if (!addBlockedWebsiteButton) {
+        console.error("Add button is null");
+        return;
+      }
+      addBlockedWebsiteButton.click();
     }
   });
 };
@@ -18,11 +33,23 @@ const clickButtonOnEnter = () => {
 clickButtonOnEnter();
 
 // Makes the input field to be focused when the options page is opened
-document.getElementById("add-website-input").focus();
+const addWebsiteInput = document.getElementById("add-website-input");
+if (!addWebsiteInput) {
+  console.error("Add website input is null");
+} else addWebsiteInput.focus();
 
 // === Functions === //
 const addWebsite = () => {
-  let websiteToRedirect = document.getElementById("add-website-input").value;
+  let addWebsiteInput: HTMLInputElement = document.getElementById(
+    "add-website-input"
+  ) as HTMLInputElement;
+
+  if (!addWebsiteInput) {
+    console.error("Website input is null");
+    return;
+  }
+
+  let websiteToRedirect = addWebsiteInput.value;
 
   if (websiteToRedirect === "") {
     return;
@@ -44,7 +71,7 @@ const addWebsite = () => {
   });
 };
 
-const removeProtocolFromWebsite = (website) => {
+const removeProtocolFromWebsite = (website: string) => {
   return website.replace(/(^\w+:|^)\/\//, "");
 };
 
@@ -54,7 +81,7 @@ const createListWithSavedOptions = () => {
   });
 };
 
-const addValue = (newValue) => {
+const addValue = (newValue: string) => {
   chrome.storage.sync.get("sitesToRedirect").then((oldSites) => {
     let newList = [...oldSites.sitesToRedirect, newValue];
     chrome.storage.sync.set({ sitesToRedirect: newList }, () => {
@@ -63,10 +90,10 @@ const addValue = (newValue) => {
   });
 };
 
-const removeValue = (keyToRemove) => {
+const removeValue = (keyToRemove: string) => {
   chrome.storage.sync.get("sitesToRedirect").then((oldSites) => {
     let newList = [
-      ...oldSites.sitesToRedirect.filter((x) => x !== keyToRemove),
+      ...oldSites.sitesToRedirect.filter((x: string) => x !== keyToRemove),
     ];
     chrome.storage.sync.set({ sitesToRedirect: newList }, () => {
       createListOfRedirectedSites(newList);
@@ -74,15 +101,18 @@ const removeValue = (keyToRemove) => {
   });
 };
 
-const createListOfRedirectedSites = (websitesToRedirect) => {
+const createListOfRedirectedSites = (websitesToRedirect: string[]) => {
   console.log("Running create list...");
   var listContainer = document.getElementById("listContainer");
+  if (!listContainer) {
+    console.error("List container is null");
+    return;
+  }
   listContainer.innerHTML = "";
   var blockedSitesDiv = document.createElement("div");
   websitesToRedirect.forEach(function (item) {
     var blockedSiteItem = document.createElement("p");
     blockedSiteItem.className = "blocked-site";
-    var removeIcon = createRemoveIcon();
     blockedSiteItem.addEventListener("click", function () {
       console.log("Remove button clicked!");
       removeValue(item);
@@ -103,6 +133,6 @@ const createRemoveIcon = () => {
   return removeIcon;
 };
 
-const isEmpty = (obj) => {
+const isEmpty = (obj: any) => {
   return Object.keys(obj).length === 0;
 };
